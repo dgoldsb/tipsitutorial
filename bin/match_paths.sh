2e3b0d98870c5ec1b4ca08045b4b7334918ad9df
dylan-carbon <dgoldsb@live.nl>
2016-06-13 16:34:38 +0200

    Ensured the order is correct of the iterations in the filename

--- bin/match_paths.sh
+++ bin/match_paths.sh
@@ -17,13 +17,18 @@ for file in */*/PARENT ; do
 		in1=${array1[0]}
 		array2=($DIR/$value/*.xtc)
 		in2=${array2[0]}
-		outname=./totalpath_$(basename $(dirname $DIR))_$(basename $(dirname $value)).xtc
-		echo $outname	
+		
 		# Check which one is forward, thus last!
 		if [ -f "$DIR/FORWARD" ]
 		then
+			outname=./totalpath_$(basename $(dirname $value))_$(basename $(dirname $DIR)).xtc
+	                echo $outname   
+
 			trjcat -f $in2 $in1 -o $outname
 		else
+			outname=./totalpath_$(basename $(dirname $DIR))_$(basename $(dirname $value)).xtc
+                	echo $outname   
+
 			trjcat -f $in1 $in2 -o $outname
 		fi
 
