f8fb71b739253bfe49bfe7b03af55a6122295af3
dylan-carbon <dgoldsb@live.nl>
2016-06-20 20:51:36 +0200

    Reverse paths dont work

--- bin/match_paths.sh
+++ bin/match_paths.sh
@@ -3,12 +3,13 @@
 add_to_traj(){
         if [ -f "./$outname" ]
         then
-                echo "Add two to each other" >> mergelog.txt 2>&1
+                echo -n "."
                 trjconv -dump $timestep -f $trajectory_trr -o temp.trr >> mergelog.txt 2>&1
 		trjcat -f $outname temp.trr -o $outname >> mergelog.txt 2>&1
-		rm temp.trr
+		rm temp.trr >> mergelog.txt 2>&1
         else
-                echo "Create the first frame" >> mergelog.txt 2>&1
+                echo "Create the first frame for $outname, now appending"
+		echo "trjconv -dump $timestep -f $trajectory_trr -o $outname" >> mergelog.txt 2>&1
 		trjconv -dump $timestep -f $trajectory_trr -o $outname >> mergelog.txt 2>&1
         fi
 }
@@ -28,40 +29,38 @@ process_accept(){
                 trajectory_trr=$DIR/${BASH_REMATCH[2]}
 		ext='.trr'
                 trajectory_xtc=${trajectory_trr%$ext}.xtc
-                add_to_traj
+                re='-*[0-9]+.[0-9]+'
+		if [[ $timestep =~ $re ]] ; then
+			add_to_traj
+		fi
         done <"$file"
-        echo "Written as ./$outname" >> mergelog.txt 2>&1
+        echo " done, written as ./$outname!"
+	ext='.trr'
+        echo "Making an .xtc of the .trr..."
+        trjconv -f ./$outname -s ../md.tpr -pbc mol -center -ur compact -o ${outname%$ext}.xtc < trjconvopts.txt >> mergelog.txt 2>&1
 }
 
-rm mergelog.txt
+rm mergelog.txt *temp* totalpath_run*
+echo "Make sure there are no files called totalpath_runx.trr!"
 # we first find any folder that is produced by TIPSI
 for file in */*/PARENT ; do
     : # $file is the PARENT file found
-	echo "Found file with a parent: $file" >> mergelog.txt 2>&1
+	echo "Found file with a parent: $file"
 	
 	# we get the directory of this file
 	DIR=$(dirname "${file}")
-        echo "Found file in directory $DIR" >> mergelog.txt 2>&1
+        echo "Found file in directory $DIR"
 
 	# if the path was not accepted, we can stop
         if [ -f "$DIR/ACCEPTED" ]
         then
-		echo "$file is accepted, proceeding..." >> mergelog.txt 2>&1
+		echo "$file is accepted, proceeding..."
 		try=$(basename $DIR)
 		run=$(basename $(dirname $DIR))
 		outname=./totalpath_run$run.trr                
 		process_accept
         else
-                echo "$file is rejected, skipping to next..." >> mergelog.txt 2>&1
+                echo "$file is rejected, skipping to next..."
         fi
 done
 
-for file in ./
-	if [ -f "./*.trr" ]
-	ext='.trr'
-	then
-		trjconv -f $file -s ../*.tpr -pbc mol -center -ur compact -o ${file%$ext}.xtc
-		1
-		1
-	fi
-done
