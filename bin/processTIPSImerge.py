ff3a0eb72136057cd678d8266f66420e504965fd
dgoldsb <dgoldsb@live.nl>
2016-07-01 17:21:15 +0200

    Bit of progress.

--- bin/processTIPSImerge.py
+++ bin/processTIPSImerge.py
@@ -18,11 +18,45 @@ import numpy as np
 # Open the CSV
 df = pd.read_csv(sys.argv[1], sep=',', header=0)
 print(df.columns.values)
+
+# Average path
 df.Length = df.MAX - df.MIN
-print(df.Length)
-lengths = np.average(df['Length'])
-print('Average length is '+lengths)
-print(df.Run)
+lengths = df.Length.tolist()
+print('Average length is '+str(np.sum(lengths)/len(lengths))+' frames')
+
+# Count FW over length
+FWs = df.DIR.tolist()
+counter = 0
+for direction in FWs:
+    if direction=='FW':
+        counter=counter+1
+print('FW/BW ratio is '+str(counter/len(FWs)))
+
+# Number of decorrelated paths
+no_runs=len(lengths)
+strings=df.COMP.tolist()
+no_decor=0
+taken_origins=[]
+for string in strings:
+    decorrelated = True
+    for i in range(1, no_runs+1):
+        target='-'+str(i)+'-'
+        if target is a substring in
+    if decorrelated:
+        no_decor=no_decor+1
+    
+print('Number of decorrelated paths is '+str(no_decor))
+
+# Generate a tree
+
+# DRAW A SINGLE LINE FIRST TO BOTH PNG AND EPS AND CHECK ZE OUTPUT
+
+# Start atshooting point,
+# go to min or max of original, go to the shooting point, go down, 
+# go to min or max
+# go back to shooting point
+
+
 
 #df[df['c']>.5][['b','e']]
 
